66593b2bfd96972be7fa7469da1c1a65b98354e6
Prudnikova <dakoto4ka@yandex.ru>
2023-03-22 20:41:56 +0300

    Initial commit

---
+++ src/test/java/pro/sky/calculator3/Calculator3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculator3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
